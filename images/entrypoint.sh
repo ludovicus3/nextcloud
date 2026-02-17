5e4b4873adf495640e486ef3e4f12318c407641f
Jason Galens <jason@galensfamily.org>
2026-02-17 05:12:50 +0000

    Drop unused variables

--- images/entrypoint.sh
+++ images/entrypoint.sh
@@ -56,27 +56,24 @@ run_path() {
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-    local var="$1"
-    local fileVar="${var}_FILE"
-    local def="${2:-}"
-    local varValue=$(env | grep -E "^${var}=" | sed -E -e "s/^${var}=//")
-    local fileVarValue=$(env | grep -E "^${fileVar}=" | sed -E -e "s/^${fileVar}=//")
-    if [ -n "${varValue}" ] && [ -n "${fileVarValue}" ]; then
-        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-        exit 1
-    fi
-    if [ -n "${varValue}" ]; then
-        export "$var"="${varValue}"
-    elif [ -n "${fileVarValue}" ]; then
-        export "$var"="$(cat "${fileVarValue}")"
-    elif [ -n "${def}" ]; then
-        export "$var"="$def"
-    fi
-    unset "$fileVar"
+  local var="$1"
+  local fileVar="${var}_FILE"
+  local def="${2:-}"
+  local varValue=$(env | grep -E "^${var}=" | sed -E -e "s/^${var}=//")
+  local fileVarValue=$(env | grep -E "^${fileVar}=" | sed -E -e "s/^${fileVar}=//")
+  if [ -n "${varValue}" ] && [ -n "${fileVarValue}" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  fi
+  if [ -n "${varValue}" ]; then
+    export "$var"="${varValue}"
+  elif [ -n "${fileVarValue}" ]; then
+    export "$var"="$(cat "${fileVarValue}")"
+  elif [ -n "${def}" ]; then
+    export "$var"="$def"
+  fi
+  unset "$fileVar"
 }
-
-uid="$(id -u)"
-gid="$(id -g)"
 
 # If another process is syncing the html folder, wait for
 # it to be done, then escape initalization.
